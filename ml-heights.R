3ebaba6e95c0ece54da8e21abe090fdaf0c6bb05
Tu Hu <tu@jingyaohu.com>
2019-01-03 13:16:53 +0100

    assignment of chapter 56, HarvardX Data Science

--- ml-heights.R
+++ ml-heights.R
@@ -6,6 +6,25 @@ data(heights)
 x <- heights$height
 y <- heights$sex
 
-#splite the heights into training set & test set
+#split data1 - generate index
 set.seed(1)
-index <- createDataPartition(y)
+train_index <- createDataPartition(y,times=1,p=.8,list=FALSE)
+
+#split data2 - data reshaping
+x_train <- x[train_index]
+x_test <- x[-train_index]
+
+y_train <- y[train_index]
+y_test <- y[-train_index]
+
+#algorithm1 - guessing
+y_hat1 <- sample(c('Male','Female'),length(x_test),replace=TRUE) %>% factor()
+#algorithm1 - assess
+result1 <- mean(y_hat1 == y_test)
+
+#algorithm2 - average height
+  #We have this insight: male's height is on average than female of this dataset
+  heights %>% group_by(sex) %>% summarise(mean(height),sd(height))
+  #We build an algorithm that classify heights higher than 62 inch (2 sd lower than mean) as male
+  y_hat2 <- ifelse(x_test>62,'Male','Female') %>% factor()
+  #Assess this algorithm by 
