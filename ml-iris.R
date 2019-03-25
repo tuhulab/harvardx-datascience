81325f1e97770fb214ce3aa3268ebbb41d4db7e9
Tu Hu <tu@jingyaohu.com>
2019-03-25 08:19:05 +0100

    Update ml-iris.R

--- ml-iris.R
+++ ml-iris.R
@@ -13,17 +13,60 @@ train <- iris[-test_index,]
 train %>% group_by(Species) %>%
   summarize(mean(Sepal.Length),mean(Sepal.Width),mean(Petal.Length),mean(Petal.Width))
 
+### train set
+sepal_len_cutoff <- seq(min(train$Sepal.Length), max(train$Sepal.Length), by = 0.1)
+sepal_wid_cutoff <- seq(min(train$Sepal.Width), max(train$Sepal.Width), by = 0.1)
+petal_len_cutoff <- seq(min(train$Petal.Length), max(train$Petal.Length), by = 0.1)
+petal_wid_cutoff <- seq(min(train$Petal.Width), max(train$Petal.Width), by = 0.1)
+sepal_len_accuracy <- map_dbl(sepal_len_cutoff, function(x){
+  y_hat <- ifelse(train$Sepal.Length > x, 'virginica', 'versicolor') %>% 
+    factor(levels = levels(test$Species))
+  mean(y_hat == train$Species)
+})
+sepal_wid_accuracy <- map_dbl(sepal_wid_cutoff, function(x){
+  y_hat <- ifelse(train$Sepal.Width > x, 'virginica', 'versicolor') %>% 
+    factor(levels = levels(test$Species))
+  mean(y_hat == train$Species)
+})
+petal_len_accuracy <- map_dbl(petal_len_cutoff, function(x){
+  y_hat <- ifelse(train$Petal.Length > x, 'virginica', 'versicolor') %>% 
+    factor(levels = levels(test$Species))
+  mean(y_hat == train$Species)
+})
+petal_wid_accuracy <- map_dbl(petal_wid_cutoff, function(x){
+  y_hat <- ifelse(train$Petal.Width > x, 'virginica', 'versicolor') %>% 
+    factor(levels = levels(test$Species))
+  mean(y_hat == train$Species)
+})
 
-sep_len_cutoff <- seq(from=min(train$Sepal.Length),
-    to=max(train$Sepal.Length),by = 0.1)
+max(sepal_len_accuracy)
+max(sepal_wid_accuracy)
+max(petal_len_accuracy)
+max(petal_wid_accuracy)
 
-sep_len_compare <- function(value=...){
-  ifelse(value>sep_len_cutoff,'virginica','versicolor')
-}
-sapply(train$Sepal.Length,sep_len_compare)
+###test the opt train data
+petal_len_opt_cutoff <- petal_len_cutoff[which.max(petal_len_accuracy)]
 
-sep_len_accuracy <- map_dbl(sep_len_cutoff, function(x){
-  y_hat <- ifelse(train$Sepal.Length > x, "Versicolor", "Virginica") %>% 
-    factor(levels = levels(factor(y)))
-  mean(y_hat == y)
+y_hat <- ifelse(test$Petal.Length > petal_len_opt_cutoff, 'virginica', 'versicolor') %>% 
+    factor(levels = levels(test$Species))
+mean(y_hat == test$Species)
+
+
+###test set
+# foo <- function(x){
+#   rangedValues <- seq(range(x)[1],range(x)[2],by=0.1)
+#   sapply(rangedValues,function(i){
+#     y_hat <- ifelse(x>i,'virginica','versicolor')
+#     mean(y_hat==test$Species)
+#   })
+# }
+# predictions <- apply(test[,3:4],2,foo)
+# sapply(predictions,max)	
+
+petal_len_cutoff <- seq(range(train$Petal.Length)[1],range(train$Petal.Length)[2],by=0.1)
+petal_wid_cutoff <- seq(range(train$Petal.Width)[1],range(train$Petal.Width)[2],by=0.1)
+combined_accuracy <- map_dbl(petal_wid_cutoff, function(x){
+  y_hat <- ifelse((train$Petal.Width > x) OR (train$Petal.Width>x), 'virginica', 'versicolor') %>% 
+    factor(levels = levels(test$Species))
+  mean(y_hat == train$Species)
 })
