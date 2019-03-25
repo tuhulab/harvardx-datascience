aafbfa7434cc3a930f79f05b377cba26f55df4d8
Tu Hu <tu@jingyaohu.com>
2019-03-25 08:18:24 +0100

    Update ml-iris.R

--- ml-iris.R
+++ ml-iris.R
@@ -10,11 +10,20 @@ test <- iris[test_index,]
 train <- iris[-test_index,]
 
 #which X has greatest overall accuracy
+train %>% group_by(Species) %>%
+  summarize(mean(Sepal.Length),mean(Sepal.Width),mean(Petal.Length),mean(Petal.Width))
+
 
 sep_len_cutoff <- seq(from=min(train$Sepal.Length),
-    to=max(train$Sepal.Length),length.out=10)
+    to=max(train$Sepal.Length),by = 0.1)
+
+sep_len_compare <- function(value=...){
+  ifelse(value>sep_len_cutoff,'virginica','versicolor')
+}
+sapply(train$Sepal.Length,sep_len_compare)
 
 sep_len_accuracy <- map_dbl(sep_len_cutoff, function(x){
-  y_hat <- ifelse(train$Sepal.Length > x, "Versicolor", "Virginica") #%>% factor(levels = levels(factor(y)))
-  #mean(y_hat == y)
+  y_hat <- ifelse(train$Sepal.Length > x, "Versicolor", "Virginica") %>% 
+    factor(levels = levels(factor(y)))
+  mean(y_hat == y)
 })
